429f2b375ff5dd04ffb24665381bef6322ed5581
Scottie Enriquez <scottie@enriquez.xxx>
2020-11-29 12:05:01 -0600

    Adding initial database template, SQL, and local deployment script

---
+++ database/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO entity(column1, column2, column3)
+VALUES ('row1-column1-data', 'row1-column2-data', 'row1-column3-data');
+
+INSERT INTO entity(column1, column2, column3)
+VALUES ('row2-column1-data', 'row2-column2-data', 'row2-column3-data');
+
+INSERT INTO entity(column1, column2, column3)
+VALUES ('row3-column1-data', 'row3-column2-data', 'row3-column3-data');
---
+++ local/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+aws cloudformation deploy --template-file ../database/rds-postgresql-template.yml \
+--stack-name rds-postgresql-$RANDOM \
+--parameter-overrides file://parameter-overrides.json \
+--tags Environment=Development
